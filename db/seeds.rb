711eac29bbfd99c0a1d61e6276b0e601e5498baa
Nikita Avvakumov <nikitaavvakumov@gmail.com>
2015-10-28 14:36:03 +0200

    Seed the db with 1000 customers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+if Rails.env == 'development'
+  puts 'Seeding the db'
+
+  puts 'Customers:'
+  1000.times do |n|
+    FactoryGirl.create :customer
+    puts n if n > 0 && n % 100 == 0
+  end
+end
